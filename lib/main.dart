f6d30230fd3d72dac881586ef21b95e992fc1b7c
carlossantos2 <carlos.santos2@ufrpe.br>
2021-07-17 22:17:40 -0300

    Ajustes para publicação

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterFire Samples',
+      title: 'Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
